b520148eae5b75e6efc0eb26a48300495f715bc0
Wheelwright <WheelwrightDL@ad.mtc.byu.edu>
2017-07-10 09:39:04 -0600

    using services to add ingredients to shopping list; no logic used to prevent mutliple items being added to list of the same type

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,36 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
-        new Recipe('Another Test Recipe', 'This is another test', 'https://cdn.pixabay.com/photo/2015/07/01/23/03/cocktail-828182_960_720.jpg')
+        new Recipe(
+            'Test Recipe', 
+            'This is simply a test', 
+            'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
+            [
+                new Ingredient('Bread', 3),
+                new Ingredient('Tomatoes', 4)]),
+        new Recipe(
+            'Another Test Recipe', 
+            'This is another test', 
+            'https://cdn.pixabay.com/photo/2015/07/01/23/03/cocktail-828182_960_720.jpg',
+            [
+                new Ingredient('Some Stuff', 2),
+                new Ingredient('More Sweet', 5)])
     ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
