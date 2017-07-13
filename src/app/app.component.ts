d0a49601e819bbf2ac6866ecb28aedc339b61554
Jones <JonesKA@ad.mtc.byu.edu>
2017-07-13 15:14:15 -0600

    added email/password authentication to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipes';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyD1ilqBH8u0YGMFc6C-JbqpQtHVsNtH7lY",
+      authDomain: "my-recipes-72e90.firebaseapp.com"
+    });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
